d24dc8c08289aa813f499a939de23cc4053d368d
rLry <lryzc@qq.com>
2024-12-28 15:55:29 +0800

    fix: relative Line Numbers

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -302,7 +302,8 @@
           conf-mode toml-ts-mode
           yaml-mode yaml-ts-mode)
          . display-line-numbers-mode)
-  :init (setq display-line-numbers-width-start t))
+  :init (setq display-line-numbers-width-start t)
+  :config (setq display-line-numbers-type 'relative))
 
 ;; Suppress GUI features
 (setq use-file-dialog nil
